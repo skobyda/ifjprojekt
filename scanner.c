a6140ceb21175638febdaef6fefdc6fd28eec31e
xDaile <43553977+xDaile@users.noreply.github.com>
2018-10-18 00:21:05 +0200

    Add files via upload
    par blbosti co som zacal... nic s testami... upravil som lexemy, vyhodil som semicolon a " <- tieto debiliny... nebude ich treba myslim si... pridal som jeden lexem ktorym si do hlavnej funkcie budem len posielat ako keby inf ze sa stalo cosi ....co musim riesit...

--- scanner.c
+++ scanner.c
@@ -8,7 +8,7 @@
  *  Denis Filo
  *
  * FILE: scanner.c
- *  FILE AUTHOR: 
+ *  FILE AUTHOR:
  */
 
 /***SYSTEM FILES***/
@@ -34,13 +34,111 @@ Scanner(FILE *sourceCode) {
     return true;
 }
 
-/* 
+
+/*
  * Returns: The next token.
  * If there are no more tokens, returns NULL
  */
+
+ static lexems ScannerGetLex(FILE *sourceCode) {
+   while(1)
+     {char c;
+       c=getchar();
+       switch(c)
+{
+        case '=': {
+          char next=getchar();
+          if(next=='=')
+            return 25;//==
+          else {
+          ungetc( next,  sourceCode);
+            return 26;//=
+          }
+        }
+
+        case '!': {
+          char next=getchar();
+          if(next=='=')
+            return 27;//!=
+          else
+            return -1;//CHYBA
+        }
+
+        case '+':
+          return 9;
+
+        case '-':
+          return 10;
+
+        case '*':
+          return 20;
+
+        case '/': {
+          char next=getchar();
+          if(next !=' ' || next <'1' || next>'9'){
+          ungetc( next,  sourceCode);
+            return -1;//CHYBA
+          }
+          return 21 ;//DIVISION
+        }
+
+        case '\n':
+          return 0;
+
+        case ',':
+          return 17;
+        case '(':
+          return 7;
+        case ')':
+          return 8;
+        case '<': {
+          char next=getchar();
+          if(next=='='){
+            return 23;
+          }
+          else{
+            ungetc( next,  sourceCode);
+            return 21;
+          }
+
+        case '>': {
+          char next=getchar();
+          if(next=='='){
+            return 24;
+          }
+          else {
+          ungetc( next,  sourceCode);
+            return 22;
+          }
+        }
+        case ' ':
+          continue;
+        case EOF:
+          return 1;
+        default : {
+          //ocividne to bude cosi ine ako jedno/dvoj znakove debiliny
+      //    ungetchar();
+          return 29;
+
+         }
+}}
+}
+
+}
+
+
 TokenPtr
 ScannerGetToken() {
     TokenPtr Token = NULL;
+
+
+
+
+
+
+
+
+
 
     //TODO
 
