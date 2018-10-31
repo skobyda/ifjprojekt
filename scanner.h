2f6ea6b07a968cef70c0653365646e6442f797b2
Michal <mychalze@gmail.com>
2018-10-31 19:29:21 +0100

    update scanner

--- scanner.h
+++ scanner.h
@@ -13,51 +13,52 @@
 
 #ifndef SCANNER_H
 #define SCANNER_H
-
-/***SYSTEM FILES***/
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef enum {
-    EOL=0,
-    EOFile,
-    ID,
-    STRING,
-    INT,
-    FLOAT,
-    NIL,
+    EOL=0,//OK        //ok
+    EOFile=1,         //ok
+    ID=2,             //
+    STRING=3,         //nok but little ok
+    INT=4,            //
+    FLOAT=5,          //
+    NIL=6,            //
     //SYMBOLS
-    LEFT_B,//(
-    RIGHT_B,//)
-    PLUS,
-    MINUS,
+    LEFT_B=7,//(      // ok
+    RIGHT_B=8,//)     //ok
+    PLUS=9,           //ok
+    MINUS=10,         //ok
+    MULTIPLY=11,      //ok
+    DIVISION=12,// /  //
+    LESS=13,//<       //ok
+    MORE=14,//>       //ok
+    LESSEQ=15,//<=    //ok
+    MOREEQ=16,//>=    //ok
+    EQ=17,//==        //ok
+    ADDITION=18, //=  //ok
+    NOTEQ=19, // !=   //ok
     //keywords
-    DEF,
-    DO,
-    ELSE,
-    END,
-    IF,
-    NOT,
-    SEMICOL,//SEMICOLON ';'
-    COMA,//,
-    THEN,
-    WHILE,
-    MULTIPLY,
-    DIVISION,// /
-    LESS,//<
-    MORE,//>
-    LESSEQ,//<=
-    MOREEQ,//>=
-    EQ,//==
-    ADDITION, //=
-    NOTEQ, // !=
-    FUNCTION,//
-    QUOTMARK,//"
-    EOL_ENUM,//HOW MUCH ITEMS IS THERE FOR TESTING
+    DEF=20,           //ok
+    DO=21,            //ok
+    ELSE=22,          //ok
+    END=23,           //ok
+    IF=24,            //ok
+    NOT=25,           //ok
+    THEN=26,          //ok
+    WHILE=27,         //ok
+    COMA=28,//,       //ok
+    FUNCTION=29,//    //
+    NEXT=30, //for some functions only in lex. analyser
+//    QUOTMARK,//"
+
+    //EOL_ENUM,//HOW MUCH ITEMS IS THERE FOR TESTING
 	PROBLEM = -1
 } lexems;
-
+lexems ScannerGetLex(FILE *sourceCode);
+int ScannerTestKeyWord(FILE *sourceCode);
 /*Structure for work with strings*/
 typedef struct {
 	lexems lexem;   // type of token
@@ -67,7 +68,10 @@ typedef struct {
 typedef Token *TokenPtr;
 
 bool Scanner(FILE *source_code);
+/*static*/
 
+/*static*/
+int ScannerTestWord(char *str, FILE *sourceCode);
 TokenPtr ScannerGetToken();
 
 #endif
