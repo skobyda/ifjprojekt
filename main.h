48c973342f59a2af54342fe20ca1dab388bd7fda
Simon Kobyda <skobyda@redhat.com>
2018-12-04 22:23:25 +0100

    nobody reads this
    Signed-off-by: Simon Kobyda <skobyda@redhat.com>

---
+++ main.h
@@ -0,0 +1,22 @@
+/* IFJ Project 2018/19
+ *
+ * TEAM:
+ *  Simon Kobyda
+ *  Michal Zelenak
+ *  Juraj Samuel Salon
+ *  Denis Filo
+ *
+ * FILE: main.h
+ *  FILE AUTHOR: Simon Kobyda
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+/* @val return value
+ * @line if 0, no line is given in print message
+ * @message return value
+ * */
+void PrintError(int val, unsigned int line, const char *message);
+
+#endif
